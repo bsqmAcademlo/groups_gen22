07d038d5e4458ffd73d1f9736c835075b4ae2460
bsqmAcademlo <brayan.munoz@academlo.com>
2023-01-05 19:04:15 -0500

    firts commit

---
+++ main.js
@@ -0,0 +1,424 @@
+const groups = {
+    noGroup: [
+        {
+            status: "",
+            name: "Rubén Hernández Céspedes",
+            email: "hernandezz07@hotmail.com",
+        },
+        {
+            status: "",
+            name: "Angelo Valdivia Jara ",
+            email: "angelovaldiviajara@gmail.com",
+        },
+        {
+            status: "",
+            name: "Guillermo Luis Benitez Barrios",
+            email: "guillermo-benitez@outlook.com",
+        },
+        {
+            status: "",
+            name: "Javier Mauricio Mosquera Vargas",
+            email: "mosquera36@hotmail.com",
+        },
+    ],
+    group: [
+        {
+            name: "grupo 1",
+            subGroup: [
+                {
+                    status: "",
+                    nameSubGroup: "grupo 1A",
+                    integrants: [
+                        {
+                            name: "Cristian Perez",
+                            email: "norberto.cp@hotmail.com",
+                        },
+                        {
+                            name: "Alex Jose Pacheco Mavarez",
+                            email: "alexpacheco407@gmail.com",
+                        },
+                        {
+                            name: "Luis Alberto Gutierrez Anicama",
+                            email: "gutierrezanicamaluisalberto@hotmail.com",
+                        },
+                        {
+                            name: "Alejandro Aguilar Medina",
+                            email: "almedina1601@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 1B",
+                    integrants: [
+                        {
+                            name: "Diego Archibald Jaldin Quintanilla",
+                            email: "diego.jaldin.q@icloud.com",
+                        },
+                        {
+                            name: "Romario Ariza Tuesta",
+                            email: "romarioariza@gmail.com",
+                        },
+                        {
+                            name: "Jean Lucas Cervantes Cárdenas",
+                            email: "jean3345641@hotmail.com",
+                        },
+                        {
+                            name: "Heri Esmer Espinosa Reyes",
+                            email: "espinosa_reyes@hotmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 1C",
+                    integrants: [
+                        {
+                            name: "Hector Leonel Soto",
+                            email: "whiterwordgame@hotmail.com",
+                        },
+                        {
+                            name: "Jamar Odalys Macias Hurtado",
+                            email: "jamarodalis@gmail.com",
+                        },
+                        {
+                            name: "Derian Augusto Arias Ballesteros",
+                            email: "derianaugustoarias@gmail.com",
+                        },
+                        {
+                            name: "Mateo Amaya Iguita",
+                            email: "mateooamaya65@gmail.com",
+                        },
+                        {
+                            name: "Carlos Ruiz",
+                            email: "ruboca1980@gmail.com",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "grupo 2",
+            subGroup: [
+                {
+                    status: "",
+                    nameSubGroup: "grupo 2A",
+                    integrants: [
+                        {
+                            name: "Jeremi Jared Castellano Herrera",
+                            email: "jeremijared10@gmail.com",
+                        },
+                        {
+                            name: "Sara Valentina Ospina Aguilar",
+                            email: "saospina099@gmail.com",
+                        },
+                        {
+                            name: "Roxana Rodríguez Revolleda",
+                            email: "roxana.rod.rev@gmail.com",
+                        },
+                        {
+                            name: "Aaron Patricio Mejía Burbano",
+                            email: "aaronburbanoms15@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 2B",
+                    integrants: [
+                        {
+                            name: "José David Guitierrez Engaiba",
+                            email: "cejonchis.14@gmail.com",
+                        },
+                        {
+                            name: "Luciano Facundo Tito Cedrón",
+                            email: "lucianotitocedron@gmail.com",
+                        },
+                        {
+                            name: "Carolina Tobon Carmona",
+                            email: "carolatcm73@outlook.com",
+                        },
+                        {
+                            name: "Luisa Elena Rivas Bacalao",
+                            email: "luelriba@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 2C",
+                    integrants: [
+                        {
+                            name: "Leonardo Grundy Paniagua",
+                            email: "encode9701@gmail.com",
+                        },
+                        {
+                            name: "Juan Wilber Alvarez Pacho",
+                            email: "ing.alvarez.mt@gmail.com",
+                        },
+                        {
+                            name: "Oliver Rodriguez Bolaños",
+                            email: "mawiis455@gmail.com",
+                        },
+                        {
+                            name: "José Daniel Rojas Caballero",
+                            email: "jdanielrojas16@gmail.com",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "grupo 3",
+            subGroup: [
+                {
+                    status: "",
+                    nameSubGroup: "grupo 3A",
+                    integrants: [
+                        {
+                            name: "William Rodríguez",
+                            email: "williamjrr.wr@gmail.com",
+                        },
+                        {
+                            name: "Max Gregor Yoel Cereceda Carbajal",
+                            email: "cereceda1991@gmail.com",
+                        },
+                        {
+                            name: "Santiago Nicolas Galván Martínez",
+                            email: "santiagon.g.m@gmail.com",
+                        },
+                        {
+                            name: "Jhon Alexander Gonzales Ramirez",
+                            email: "jhongonzalez10-03@hotmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 3B",
+                    integrants: [
+                        {
+                            name: "Luis Daniel Rojas Urdaneta",
+                            email: "v26396341@gmail.com",
+                        },
+                        {
+                            name: "Gissel Vanessa Cabascango Anrango",
+                            email: "gissel.cabascango@yachaytech.edu.ec",
+                        },
+                        {
+                            name: "Elías Fernando Reyes Adarme",
+                            email: "reyesfernando001@gmail.com",
+                        },
+                        {
+                            name: "Daniel Felipe Calderón",
+                            email: "dc7027233@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 3C",
+                    integrants: [
+                        {
+                            name: "Robert Arjona",
+                            email: "Encpaid@gmail.com",
+                        },
+                        {
+                            name: "Julian Andres Guayanes Maldonado",
+                            email: "julianguayanes4@gmail.com",
+                        },
+                        {
+                            name: "César Rojas Ore",
+                            email: "rasecs.2010@gmail.com",
+                        },
+                        {
+                            name: "Miller Javier Suarez Cantillo",
+                            email: "mjsc_1@live.com",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "grupo 4",
+            subGroup: [
+                {
+                    status: "",
+                    nameSubGroup: "grupo 4A",
+                    integrants: [
+                        {
+                            name: "Adderly Orizano Salvador",
+                            email: "adderlyorizano@gmail.com",
+                        },
+                        {
+                            name: "José Aldair Alburqueque Gutierrez",
+                            email: "aldairalburqueque8@gmail.com",
+                        },
+                        {
+                            name: "Miguel Angel González Velez",
+                            email: "mgonzalez94@hotmail.es",
+                        },
+                        {
+                            name: "Emerson Wilson Condori Yujra",
+                            email: "escanorrosa123@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 4B",
+                    integrants: [
+                        {
+                            name: "Alex Yonathan Puicon Rubira",
+                            email: "alexyonathan@gmail.com",
+                        },
+                        {
+                            name: "Keiyis Lizeth Oviedo Pérez",
+                            email: "keiyisoviedo@gmail.com",
+                        },
+                        {
+                            name: "José Valentín Ramos Meza",
+                            email: "jvalentin.plt@hotmail.com",
+                        },
+                        {
+                            name: "Matias Gabriel Orlando Carabajal",
+                            email: "matias1922@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 4C",
+                    integrants: [
+                        {
+                            name: "Mijail Lazcano Ballesteros",
+                            email: "mishco911@gmail.com",
+                        },
+                        {
+                            name: "Manuel Alejandro Guarena Hernandez",
+                            email: "blackperseus999@gmail.com",
+                        },
+                        {
+                            name: "Eliseo Antonio Torres Vique",
+                            email: "felipe.torres95@gmail.com",
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "grupo 5",
+            subGroup: [
+                {
+                    status: "",
+                    nameSubGroup: "grupo 5A",
+                    integrants: [
+                        {
+                            name: "José Carlos Gonzalez Jurado",
+                            email: "jose.gjurado@gmail.com",
+                        },
+                        {
+                            name: "Edwin Yesid Ariza Martinez",
+                            email: "edwinariza05@hotmail.com",
+                        },
+                        {
+                            name: "LUIS FABIAN OCHOA ANGULO",
+                            email: "fabianochoa025@gmail.com",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 5B",
+                    integrants: [
+                        {
+                            name: "Manuela Salazar Becerro",
+                            email: "maslazar1995@gmail.com",
+                        },
+                        {
+                            name: "JOSÉ ENRIQUE VERA PICO",
+                            email: "cheovera30526@gmail.com",
+                        },
+                        {
+                            name: "Josue Moya",
+                            email: "molina-2011-2012@hotmail.es",
+                        },
+                    ],
+                },
+                {
+                    status: "",
+                    nameSubGroup: "grupo 5C",
+                    integrants: [
+                        {
+                            name: "Leonardo Emanuel Salvador Barboza",
+                            email: "leonardo_capricornio_18@hotmail.com",
+                        },
+                        {
+                            name: "Abel Gabriel Gayebor Peñafiel",
+                            email: "tomarsz@hotmail.com",
+                        },
+                        {
+                            name: "Juan David Reyes Sanchez",
+                            email: "judare85@hotmail.com",
+                        },
+                        {
+                            name: "Wilmer Duarte",
+                            email: "wilduarte.d@gmail.com",
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
+const containerUsers = document.querySelector(".containerUsers");
+const containerNoGroup = document.querySelector(".containerNoGroup");
+
+function printIntegrants(integrans) {
+    let html = "";
+
+    integrans.forEach(({ name, email }) => {
+        html += `
+            <div class="integrant">
+                <p><b>${name}</b></p>
+                <p>${email}</p>
+            </div>`;
+    });
+
+    return html;
+}
+
+function printSubGroups(subGroups) {
+    let html = "";
+
+    subGroups.forEach(({ nameSubGroup, integrants }) => {
+        html += `<div class="subGroup">
+                    <h3>${nameSubGroup}</h3>
+                    <div class="integrants">
+                        ${printIntegrants(integrants)}
+                    </div>
+                </div>`;
+    });
+
+    return html;
+}
+
+function printGroups() {
+    let html = "";
+
+    groups.group.forEach(({ name, subGroup }) => {
+        html += `
+        <div class="group">
+            <h2>${name}</h2>
+            <div class="subGroups">
+                ${printSubGroups(subGroup)}
+            </div>
+        </div>
+    `;
+    });
+
+    containerUsers.innerHTML = html;
+}
+
+printGroups();
